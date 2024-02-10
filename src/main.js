9787b5fafab778dde2720fe8554157218cabdf5b
Mirror <yllikano@gmail.com>
2024-02-10 08:01:47 +0200

    module-3/ final. Аdd renderBoard and refactoring

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import FilmListView from "./view/film-list.js";
 import FilmContainerView from "./view/film-container.js";
 import FilmView from "./view/film.js";
 import FilmDetailsView from "./view/film-details.js";
+import FilmListEmpty from "./view/film-list-empty.js";
 import ShowMoreButtonView from "./view/show-more-button.js";
 import FilmTopRatedView from "./view/film-top-rated.js";
 import FilmMostCommentedView from "./view/film-most-commented.js";
@@ -26,13 +27,13 @@ const renderFilm = (filmListElement, film) => {
   const filmComponent = new FilmView(film);
   const filmDetailsComponent = new FilmDetailsView(film);
 
-  const replaceFormToCard = () => {
+  const removeFilmDetails = () => {
     filmDetailsComponent.getElement().remove();
   };
 
   const onEscKeyDown = (evt) => {
     if (evt.key === 'Escape' || evt.key === 'Esc') {
-      replaceFormToCard();
+      removeFilmDetails();
       document.removeEventListener('keydown', onEscKeyDown);
     }
   };
@@ -43,7 +44,7 @@ const renderFilm = (filmListElement, film) => {
   });
 
   filmDetailsComponent.getElement().querySelector('.film-details__close-btn').addEventListener('click', () => {
-    replaceFormToCard();
+    removeFilmDetails();
     document.removeEventListener('keydown', onEscKeyDown);
   });
 
@@ -54,38 +55,49 @@ render(siteHeaderElement, new UserProfileView().getElement(), RenderPosition.BEF
 render(siteMainElement, new FilterView(filters).getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, new SortView().getElement(), RenderPosition.BEFOREEND);
 
-const filmBoardComponent = new FilmBoardView();
-const filmListComponent = new FilmListView();
-const filmLContainerComponent = new FilmContainerView();
+const renderBoard = (boardContainer, boardFilms) => {
+  const filmBoardComponent = new FilmBoardView();
+  const filmListComponent = new FilmListView();
+  const filmLContainerComponent = new FilmContainerView();
 
-render(siteMainElement, filmBoardComponent.getElement(), RenderPosition.BEFOREEND);
-render(filmBoardComponent.getElement(), filmListComponent.getElement(), RenderPosition.BEFOREEND);
-render(filmListComponent.getElement(), filmLContainerComponent.getElement(), RenderPosition.BEFOREEND);
-render(filmBoardComponent.getElement(), new FilmTopRatedView().getElement(), RenderPosition.BEFOREEND);
-render(filmBoardComponent.getElement(), new FilmMostCommentedView().getElement(), RenderPosition.BEFOREEND);
 
-for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-  renderFilm(filmLContainerComponent.getElement(), films[i]);
+  render(boardContainer, filmBoardComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmBoardComponent.getElement(), filmListComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmListComponent.getElement(), filmLContainerComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmBoardComponent.getElement(), new FilmTopRatedView().getElement(), RenderPosition.BEFOREEND);
+  render(filmBoardComponent.getElement(), new FilmMostCommentedView().getElement(), RenderPosition.BEFOREEND);
+
+
+  if (boardFilms.length === 0) {
+    render(filmBoardComponent.getElement(), new FilmListEmpty().getElement(), RenderPosition.AFTERBEGIN);
+    return;
+  }
+
+  for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
+    renderFilm(filmLContainerComponent.getElement(), films[i]);
+  }
+
+  if (films.length > FILM_COUNT_PER_STEP) {
+    let renderedFilmCount = FILM_COUNT_PER_STEP;
+
+    const showMoreButtonComponent = new ShowMoreButtonView();
+
+    render(filmListComponent.getElement(), showMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+
+    showMoreButtonComponent.getElement().addEventListener('click', (evt) => {
+      evt.preventDefault();
+      films
+        .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
+        .forEach((film) => renderFilm(filmLContainerComponent.getElement(), film));
+
+        renderedFilmCount += FILM_COUNT_PER_STEP;
+
+      if (renderedFilmCount >= films.length) {
+        showMoreButtonComponent.getElement().remove();
+        showMoreButtonComponent.removeElement();
+      }
+    });
+  }
 }
 
-if (films.length > FILM_COUNT_PER_STEP) {
-  let renderedFilmCount = FILM_COUNT_PER_STEP;
-
-  const showMoreButtonComponent = new ShowMoreButtonView();
-
-  render(filmListComponent.getElement(), showMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
-
-  showMoreButtonComponent.getElement().addEventListener('click', (evt) => {
-    evt.preventDefault();
-    films
-      .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => renderFilm(filmLContainerComponent.getElement(), film));
-
-      renderedFilmCount += FILM_COUNT_PER_STEP;
-
-    if (renderedFilmCount >= films.length) {
-      showMoreButtonComponent.getElement().remove();
-      showMoreButtonComponent.removeElement();
-    }
-  });
-}
+renderBoard(siteMainElement, films)
